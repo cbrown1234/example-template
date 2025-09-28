f699beb1607040ffaec1420a6ab98cc0cc65ad4b
Chris Brown <cbrown1234@hotmail.co.uk>
2025-09-28 13:40:20 +0100

    feat: Enable multiline validation messages

--- extensions/validators.py
+++ extensions/validators.py
@@ -1,8 +1,24 @@
 """Pydantic model integration helpers."""
 
+import prompt_toolkit
 from jinja2 import Environment
 from jinja2.ext import Extension
 from pydantic import BaseModel, StrictStr, ValidationError
+
+validation_toolbar_init = prompt_toolkit.widgets.ValidationToolbar.__init__
+
+
+def validation_toolbar_init_patched(
+    self: prompt_toolkit.widgets.ValidationToolbar, *args: tuple, **kwargs: dict
+) -> None:
+    """Wrap ValidationToolbar __init__ for patching."""
+    validation_toolbar_init(self, *args, **kwargs)
+
+    # Uncap validation message length to enable multiline error messages
+    self.container.content.height = None
+
+
+prompt_toolkit.widgets.ValidationToolbar.__init__ = validation_toolbar_init_patched
 
 
 def copier_validator(model: BaseModel) -> callable:
@@ -27,7 +43,7 @@ def copier_validator(model: BaseModel) -> callable:
             model.model_validate(value, strict=True)
         except ValidationError as err:
             messages = [f'{":".join(e["loc"])}: {e["msg"]}' for e in err.errors()]
-        return ', '.join(messages)
+        return '\n'.join(messages)
 
     return validator
 
